f759e333afdc2e77635204ab1e9f4ed13e8a8408
th3-Sumit <kumarsumit747037@gmail.com>
2025-06-12 10:57:15 +0530

    changed password

--- Middleware/mailTransporter.js
+++ Middleware/mailTransporter.js
@@ -1,10 +1,13 @@
 const nodemailer = require('nodemailer')
+require('dotenv').config()
 
+const email = process.env.email || ""
+const password = process.env.email_password || ""
 const transporter = nodemailer.createTransport({
   service: "gmail", // or use SMTP server of your choice
   auth: {
-    user: "freelancersumitsingh@gmail.com", // replace with your email
-    pass: "uhla ddqb tzpz iome", // use app password for Gmail
+    user: email, // replace with your email
+    pass: password, // use app password for Gmail
   },
 });
 
